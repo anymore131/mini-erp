2fc47052a3a00512e8ea4d0aeae42f4b7462355b
王嘉豪 <11837429+guan-xue@user.noreply.gitee.com>
2024-11-17 14:01:44 +0800

    创建项目

--- customer-service/src/main/java/cn/edu/zust/se/CustomerApplication.java
+++ customer-service/src/main/java/cn/edu/zust/se/CustomerApplication.java
@@ -3,12 +3,14 @@ package cn.edu.zust.se;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableDiscoveryClient
 @SpringBootApplication
-public class GatewayApplication {
+@EnableFeignClients
+public class CustomerApplication {
     public static void main(String[] args) {
-        SpringApplication.run(GatewayApplication.class, args);
-        System.out.println("Gateway网关服务启动成功 ヾ(￣ー￣)X(^▽^)ゞ");
+        SpringApplication.run(CustomerApplication.class, args);
+        System.out.println("客户服务启动成功 ヾ(￣ー￣)X(^▽^)ゞ");
     }
 }
--- order-processing-service/src/main/java/cn/edu/zust/se/OrderProcessingApplication.java
+++ order-processing-service/src/main/java/cn/edu/zust/se/OrderProcessingApplication.java
@@ -3,12 +3,14 @@ package cn.edu.zust.se;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableDiscoveryClient
 @SpringBootApplication
-public class GatewayApplication {
+@EnableFeignClients
+public class OrderProcessingApplication {
     public static void main(String[] args) {
-        SpringApplication.run(GatewayApplication.class, args);
-        System.out.println("Gateway网关服务启动成功 ヾ(￣ー￣)X(^▽^)ゞ");
+        SpringApplication.run(OrderProcessingApplication.class, args);
+        System.out.println("订单处理服务启动成功 ヾ(￣ー￣)X(^▽^)ゞ");
     }
 }
