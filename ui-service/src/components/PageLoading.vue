6fb439e00e95dcd8c00421eadf86db2505988f6d
王嘉豪 <11837429+guan-xue@user.noreply.gitee.com>
2024-12-17 21:13:09 +0800

    1

---
+++ ui-service/src/components/PageLoading.vue
@@ -0,0 +1,38 @@
+<template>
+  <div v-if="loading" class="page-loading">
+    <el-loading
+      :element-loading-text="text"
+      element-loading-spinner="el-icon-loading"
+      element-loading-background="rgba(255, 255, 255, 0.9)"
+    />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  props: {
+    loading: {
+      type: Boolean,
+      default: false
+    },
+    text: {
+      type: String,
+      default: '加载中...'
+    }
+  }
+})
+</script>
+
+<style scoped>
+.page-loading {
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background: rgba(255, 255, 255, 0.9);
+  z-index: 100;
+}
+</style> 
